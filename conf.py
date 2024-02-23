e77b6b47886bb1298e43752f5521957d6cc3b387
上野和雅 <uenokazumasa@uenokazumasanoMacBook-Pro.local>
2024-02-23 18:33:34 +0900

    revised conf.py

--- conf.py
+++ conf.py
@@ -13,7 +13,8 @@ author = 'Kazumasa Ueno'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo', 'sphinx.ext.githubpages'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
